52f05300633ea1c9cc92322efe22dee6fd6a413c
Mika Pi <mika+github@mika.global>
2020-11-15 16:32:45 -0800

    Add MenuUi

---
+++ Source/lj2/MenuUi.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MenuUi.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LJ2_API UMenuUi : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
